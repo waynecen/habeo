a347d6a815f70c1b9d98f20ff7b7c4291588f0a9
Wayne Cen <wayne.cen@gmail.com>
2023-01-30 06:42:17 -0500

    Update styling, add default placeholder text

--- src/components/PrimaryGoalInput.jsx
+++ src/components/PrimaryGoalInput.jsx
@@ -4,6 +4,8 @@ import { useState } from "react"
 export default function PrimaryGoalInput({ id }) {
 	const [primaryGoal, setPrimaryGoal] = useState("")
 
+	const defaultPlaceholder = "Today, I want to..."
+
 	return (
 		<form>
 			<input
@@ -11,11 +13,12 @@ export default function PrimaryGoalInput({ id }) {
 				name="primaryGoal"
 				value={primaryGoal}
 				className={styles.input}
-				placeholder="Today, I want to..."
+				placeholder={defaultPlaceholder}
 				onChange={(e) => setPrimaryGoal(e.target.value)}
 				onFocus={(e) => (e.target.placeholder = "")}
-				onBlur={(e) => (e.target.placeholder = primaryGoal)}
+				onBlur={(e) => (e.target.placeholder = defaultPlaceholder)}
 				autoFocus
+				autoComplete="off"
 			/>
 		</form>
 	)
