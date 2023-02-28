99fe0e3485408e450681a49a8ba8bb083ed077cf
Wayne <wayne.cen@gmail.com>
2023-02-28 18:18:39 -0500

    style: :art: Add icon swap for password, update variable names

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -3,9 +3,12 @@ import Head from "next/head"
 import Link from "next/link"
 import Layout from "src/components/Layout"
 import Image from "next/image"
-import { RiEyeCloseLine } from "react-icons/ri"
+import { RiEyeCloseLine, RiEyeLine } from "react-icons/ri"
+import { useState } from "react"
 
 export default function SignIn() {
+	const [visible, setVisible] = useState(false)
+
 	return (
 		<Layout visible={false}>
 			<Head>
@@ -27,13 +30,13 @@ export default function SignIn() {
 					<div className={styles.input_group}>
 						<input
 							className={styles.input}
-							type="password"
+							type={`${visible ? "text" : "password"}`}
 							name="password"
 							placeholder="Password"
 							autoComplete="off"
 						/>
-						<span>
-							<RiEyeCloseLine size={21} />
+						<span onClick={() => setVisible(!visible)}>
+							{visible ? <RiEyeLine size={21} /> : <RiEyeCloseLine size={21} />}
 						</span>
 					</div>
 					<div className={styles.button_group}>
@@ -46,6 +49,7 @@ export default function SignIn() {
 								alt="Google Logo"
 								width="21"
 								height="21"
+								className={styles.logo_google}
 							/>
 							Sign in with Google
 						</button>
