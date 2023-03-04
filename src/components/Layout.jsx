cf29ca16b483712b8525d5748349a877c6da02b7
Wayne <wayne.cen@gmail.com>
2023-03-04 08:15:50 -0500

    refactor: :art: Move LoginButton component into Layout, Delete Profile page
    Added a visible prop so pages can specify whether or not login/signout button is visible, deleted profile page to work on at a later time

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,31 @@
-import styles from "@/styles/components/Layout.module.scss"
-import LoginButton from "@/components/LoginButton"
-import Link from "next/link"
+import styles from '@/styles/components/Layout.module.scss'
+import Link from 'next/link'
+import { useSession, signOut } from 'next-auth/react'
 
 export default function Layout({ children, visible }) {
+	const { data: session } = useSession()
+
 	return (
 		<div className={styles.layout_wrapper}>
 			<div className={styles.nav}>
 				<Link href="/" className={styles.logo}>
 					Habi
 				</Link>
-				{visible && <LoginButton />}
+				{session ? (
+					visible ? (
+						<button onClick={() => signOut()} className={styles.button}>
+							Sign Out
+						</button>
+					) : (
+						<></>
+					)
+				) : visible ? (
+					<Link href={'/auth/signin'} className={styles.button}>
+						Login
+					</Link>
+				) : (
+					<></>
+				)}
 			</div>
 
 			<div className={styles.children_layout}>{children}</div>
