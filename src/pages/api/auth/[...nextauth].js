9f44183c3ff1975a61a290edebac16f411894268
Wayne <wayne.cen@gmail.com>
2023-03-04 17:53:20 -0500

    feat: :sparkles: Configure auth options
    Added the credentialsProvider to allow users to login, checks for existing users and verifies by matching hash to password

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,19 +1,50 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
-import clientPromise from "@lib/mongodb"
+import clientPromise from '@lib/mongodb'
+import Users from '@database/model/userSchema'
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import connectMongo from '@database/connectMongo'
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
+const argon2 = require('argon2')
 
 export const authOptions = {
+	adapter: MongoDBAdapter(clientPromise),
+	pages: {
+		signIn: '/auth/signin',
+	},
 	providers: [
+		CredentialsProvider({
+			name: 'Credentials',
+			async authorize(credentials, req) {
+				connectMongo().catch((error) => {
+					error: 'Failed to connect to database'
+				})
+
+				// Check for existing user
+				const user = await Users.findOne({ email: credentials.email })
+				if (!user) {
+					throw new Error('User with this email does not exist')
+				}
+
+				// Verify Password
+				const verifyPassword = await argon2.verify(
+					user.password,
+					credentials.password
+				)
+				if (!verifyPassword || user.email !== credentials.email) {
+					throw new Error('Username and Password do not match.')
+				}
+
+				return user
+			},
+		}),
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
-	adapter: MongoDBAdapter(clientPromise),
-	secret: process.env.NEXTAUTH_SECRET,
-	pages: {
-		signIn: "/auth/signin",
+	session: {
+		strategy: 'jwt',
 	},
 }
 
