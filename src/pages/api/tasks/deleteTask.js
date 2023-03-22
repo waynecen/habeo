eb00fdf836c435fedecad2697c3171baeabbe605
Wayne <wayne.cen@gmail.com>
2023-03-22 13:40:50 -0400

    feat: :sparkles: Implement delete function
    Add icon to list items, add delete handler to api, update styling

--- src/pages/api/tasks/deleteTask.js
+++ src/pages/api/tasks/deleteTask.js
@@ -7,19 +7,19 @@ export default async function handler(req, res) {
 	)
 
 	try {
-		// Only accept POST request
-		if (req.method === 'POST') {
+		if (req.method === 'DELETE') {
 			if (!req.body) {
-				return res.status(404).json({ error: 'Missing field data' })
+				return res.status(404).json({ error: 'Missing data' })
 			}
 
-			// Create new instance of task
-			const task = await Task.create(req.body)
-			res.json({ task })
+			const { id } = req.body
+
+			const task = await Task.findOneAndDelete(id)
+			res.json(task)
 		} else {
 			res.status(500).json({ message: 'Invalid HTTP Method' })
 		}
 	} catch (err) {
-		return res.status(404).json({ err: 'Failed to create task' })
+		return res.status(404).json({ err: 'Failed to delete task' })
 	}
 }
