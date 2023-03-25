c4bf57840b65f8eeddd4c0b62f80743117ca2f7d
Wayne <wayne.cen@gmail.com>
2023-03-25 14:24:46 -0400

    style: :lipstick: Add favicon files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import 'styles/globals.scss'
 import { Inter } from '@next/font/google'
 import { SessionProvider } from 'next-auth/react'
+import Head from 'next/head'
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -11,10 +12,39 @@ export default function App({
 	pageProps: { session, ...pageProps },
 }) {
 	return (
-		<SessionProvider session={session}>
-			<main className={inter.className}>
-				<Component {...pageProps} />
-			</main>
-		</SessionProvider>
+		<>
+			<Head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/favicon/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/favicon/site.webmanifest" />
+				<link
+					rel="mask-icon"
+					href="/favicon/safari-pinned-tab.svg"
+					color="#2b2b2b"
+				/>
+				<meta name="msapplication-TileColor" content="#222222" />
+				<meta name="theme-color" content="#2c2c2c" />
+			</Head>
+			<SessionProvider session={session}>
+				<main className={inter.className}>
+					<Component {...pageProps} />
+				</main>
+			</SessionProvider>
+		</>
 	)
 }
