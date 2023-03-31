21fb00eee5b1c2dcf18a1a0408bdaa7e21c756de
Wayne <wayne.cen@gmail.com>
2023-03-30 20:01:55 -0400

    refactor: :art: Update Add/Delete endpoint
    Add author field to task on creation, refactor delete to be more clear

--- src/pages/api/tasks/deleteTask.js
+++ src/pages/api/tasks/deleteTask.js
@@ -1,5 +1,5 @@
 import connectMongo from 'database/connectMongo'
-import Task from 'database/model/tasks'
+import { Task } from 'database/model/users'
 
 export default async function handler(req, res) {
 	await connectMongo().catch(err =>
@@ -12,14 +12,14 @@ export default async function handler(req, res) {
 				return res.status(404).json({ error: 'Missing data' })
 			}
 
-			const { id } = req.body
-
-			const task = await Task.findOneAndDelete(id)
-			res.json(task)
+			const idToDelete = req.body
+			await Task.findById(idToDelete).deleteOne()
+			res.status(204).end()
 		} else {
-			res.status(500).json({ message: 'Invalid HTTP Method' })
+			res.status(400).json({ message: 'Invalid HTTP Method' })
 		}
 	} catch (err) {
+		console.log(err)
 		return res.status(404).json({ err: 'Failed to delete task' })
 	}
 }
